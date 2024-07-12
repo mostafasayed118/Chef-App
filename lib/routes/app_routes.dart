33313fb3035d5809fa95d1d071e60a65e6374630
Mustafa Al3tar <mustafa_620125@oi.edu.eg>
2024-07-13 01:35:57 +0300

    feat: Add authentication screens
    This commit adds the authentication screens for login, reset password, send code, change password, and splash screen. Each screen is implemented as a stateless widget and contains a placeholder container for now.

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,14 @@
+class Routes{
+  static const String initialRoute = '/';
+  static const String changeLanguage='/changeLanguage';
+  static const String login='/login';
+  static const String sendCode='/sendCode';
+  static const String resetPassword='/resetPassword';
+  static const String menuHome='/menuHome';
+  static const String addMeal='/addMeal';
+  static const String profileHome='/profileHome';
+  static const String updateProfile='/updateProfile';
+  static const String changePassword='/changePassword';
+  static const String settings='/settings';
+}
+class AppRoutes{}
