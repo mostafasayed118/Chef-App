37cc512ed3a0912869ce3722e20c0faf401502bd
Mustafa Al3tar <mustafa_620125@oi.edu.eg>
2024-07-12 20:59:13 +0300

    chore: Update assets and colors in app_strings.dart, app_assets.dart, and app_colors.dart

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,8 @@
+class AppAssets {
+  static const String basePath = 'assets/images/';
+  static const String logo = '${basePath}logo.png';
+  static const String backgroundOne = '${basePath}background_one.png';
+  static const String backgroundTwo = '${basePath}background_two.png';
+  static const String lock = '${basePath}lock.png';
+  static const String imagePicker = '${basePath}image_picker.png';
+}
---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFFA9A0C);
+  static const Color orange = Color(0xFFFB5607);
+  static const Color black = Color(0xff1E1E1E);
+  static const Color blackBottom = Color(0xff111111);
+  static const Color blackForProfile = Color(0xff242424);
+  static const Color primaryBlack = Color(0xff1E1E1E);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color greyTextFiled = Color(0xff9D9FA0);
+  static const Color greyForget = Color(0xffB5B7B8);
+  static const Color greyBlue = Color(0xff7D8FAB);
+}
