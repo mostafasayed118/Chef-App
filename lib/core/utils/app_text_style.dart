f5dd4520d6369fe341f24737a48dee0fa22420ac
Mustafa Al3tar <mustafa_620125@oi.edu.eg>
2024-07-12 22:12:02 +0300

    add app textStyle

---
+++ lib/core/utils/app_text_style.dart
@@ -0,0 +1,40 @@
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle _textStyle(
+    {required Color color,
+    required double fontSize,
+    required FontWeight fontWeight}) {
+  return GoogleFonts.lato(
+    color: color,
+    fontSize: fontSize,
+    fontWeight: fontWeight,
+  );
+}
+
+TextStyle boldStyle({
+  required Color color,
+  required double fontSize,
+}) =>
+    _textStyle(
+      color: color,
+      fontSize: fontSize,
+      fontWeight: FontWeight.bold,
+    );
+
+TextStyle semiBold({
+  required Color color,
+  required double fontSize,
+}) =>
+    _textStyle(color: color, fontSize: fontSize, fontWeight: FontWeight.w600);
+
+TextStyle regularStyle({
+  required Color color,
+  required double fontSize,
+}) =>
+    _textStyle(
+      color: color,
+      fontSize: fontSize,
+      fontWeight: FontWeight.normal,
+    );
